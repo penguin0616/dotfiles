2c5f955f85e274f5c9c846e4f5168a15aa276f27
penguin0616 <23770056+penguin0616@users.noreply.github.com>
2025-07-10 19:10:49 -0500

    Add podman aliases and remove session from rprompt

---
+++ .bashrc.d/alias/podman.sh
@@ -0,0 +1,4 @@
+# https://gist.github.com/jgrodziski/9ed4a17709baad10dbcd4530b60dfcbb
+alias p='podman'
+alias pc='podman-compose'
+#alias docker-remove-all-containers='docker stop $(docker ps -a -q); docker rm $(docker ps -a -q);'
